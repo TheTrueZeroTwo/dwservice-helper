c1f775d7ca8360a48b7f6c0806d59506b124239c
Zero Two <85081508+TheTrueZeroTwo@users.noreply.github.com>
2023-01-05 12:11:14 -0700

    Create linux-install.sh

---
+++ linux-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download dwagent.sh
+curl -O https://www.dwservice.net/download/dwagent.sh
+
+# make the script executable
+sudo chmod +x dwagent.sh
+
+# run the script with sudo
+sudo ./dwagent.sh
