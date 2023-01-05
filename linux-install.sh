fa90e1c6ccc49945d2772b073e05ae36c5b3a009
Zero Two <85081508+TheTrueZeroTwo@users.noreply.github.com>
2023-01-05 14:22:40 -0700

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download dwagent.sh
-curl -O https://www.dwservice.net/download/dwagent.sh
+wget https://www.dwservice.net/download/dwagent.sh
 
 # make the script executable
 sudo chmod +x dwagent.sh
